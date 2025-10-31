3eb4887056e2d88f647daafde8113044399c7ce8
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-10-31 20:15:59 +0200

    modify class dependencies

--- viikko1/nhl-statistics/src/index.py
+++ viikko1/nhl-statistics/src/index.py
@@ -1,8 +1,10 @@
 from statistics_service import StatisticsService
+from player_reader import PlayerReader
 
 
 def main():
-    stats = StatisticsService()
+    player_reader = PlayerReader("https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt")
+    stats = StatisticsService(player_reader)
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top(10)
 
