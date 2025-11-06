da584f19999d5631e565bc8442f0c5ffe924eb97
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-11-06 16:14:09 +0200

    clearer statistics with sorted table for players

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -13,9 +13,14 @@ def main():
 
     print("Suomalaisten pelaajien tilastot:")
 
-    for player in players:
-        if player.nationality == "FIN":
-            print(player.name, ":", player.goals, "maalia ja", player.assists, "syöttöä")
+    finnish_players = [player for player in players if player.nationality == "FIN"]
+    sorted_finns = sorted(finnish_players, key=lambda player: player.goals + player.assists, reverse=True)
+
+    print(f'{"Nimi":30} {"Maalit":>6} {"Syötöt":>8} {"Tehopisteet":>12}')   
+
+    for player in sorted_finns:
+       points = player.goals + player.assists
+       print(f'{player.name:30} {player.goals:6d} {player.assists:8d} {points:12d}')
 
 if __name__ == "__main__":
     main()
