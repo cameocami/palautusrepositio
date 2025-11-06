a331cf69742cb3a760501978e8da2ae4c8d9743f
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-11-06 16:02:01 +0200

    remove redunant prints

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,9 +5,6 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
     players = []
 
     for player_dict in response:
