d870079b1b059065bac1dfc45310c7d47d887521
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-11-08 16:19:33 +0200

    pylint fixes to player class

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,8 +1,7 @@
 
-from player import Player
 from player_reader import PlayerReader
 from player_stats import PlayerStats
-from ui import UI   
+from ui import UI  
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,13 +1,24 @@
-class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-        self.id = dict['id']
-        self.nationality = dict['nationality']
-        self.goals = dict['goals']
-        self.assists = dict['assists']
-        self.team = dict['team']
-        self.games = dict['games']
+'''Module defining the Player class representing a NHL hockey player.'''
 
+class Player:
+    '''Class representing a NHL hockey player with relevant statistics as attributes.'''
+    def __init__(self, player_dictionary: dict):
+        self.name = player_dictionary['name']
+        self.id = player_dictionary['id']
+        self.nationality = player_dictionary['nationality']
+        self.goals = player_dictionary['goals']
+        self.assists = player_dictionary['assists']
+        self.team = player_dictionary['team']
+        self.games = player_dictionary['games']
 
     def __str__(self):
+        '''Return a string representation of the player.'''
         return f'{self.name:20}: {self.goals} + {self.assists} : {self.goals + self.assists}'
+
+    def points(self):
+        '''Return the total points (goals + assists) of the player.'''
+        return self.goals + self.assists
+
+    def __lt__(self, other):
+        '''Define less-than comparison based on points for sorting players.'''
+        return self.points() < other.points()
