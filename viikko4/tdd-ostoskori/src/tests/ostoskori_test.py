2d4eef29f0dd80e937d449c67539623d92e1caf9
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-11-22 18:08:35 +0200

    extend test to price and amount of products in the shopping cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -96,3 +96,5 @@ class TestOstoskori(unittest.TestCase):
         self.kori.poista_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
+        self.assertEqual(self.kori.hinta(), 0)
