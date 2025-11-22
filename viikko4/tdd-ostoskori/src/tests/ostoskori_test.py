36867193fae7a818c3f01410a9e554e2ff44ffa8
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-11-22 18:00:57 +0200

    add test for removing one product from a purchase in shopping cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -81,3 +81,11 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
         self.assertEqual(ostokset[0].tuotteen_nimi(), "Maito")
         self.assertEqual(ostokset[0].lukumaara(), 2)
+
+    def test_yhden_tuotteen_poistamisen_jalkeen_kun_tuotetta_on_ollut_kaksi_korissa_ostokset_palauttaa_oikean_lukumaaran(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(ostokset[0].lukumaara(), 1)
