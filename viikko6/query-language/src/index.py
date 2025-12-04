8c7e933068f484cf0d8e578c6c60908c29d311ba
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-12-04 14:05:37 +0200

    create Or matcher

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
@@ -11,6 +11,28 @@ def main():
         HasAtLeast(5, "goals"),
         HasAtLeast(20, "assists"),
         PlaysIn("PHI")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("")
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("")
+    matcher = And(
+    HasAtLeast(70, "points"),
+    Or(
+        PlaysIn("COL"),
+        PlaysIn("FLA"),
+        PlaysIn("BOS")
+    )
     )
 
     for player in stats.matches(matcher):
