0242e5a8537a8b526286e04008a5059a687b8a0e
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-12-21 18:27:04 +0200

    store callables instead of instaces to avoid shared instances of ai or judge

--- viikko7/kivi-paperi-sakset/src/ui.py
+++ viikko7/kivi-paperi-sakset/src/ui.py
@@ -8,9 +8,9 @@ from tuomari import Tuomari
 class UI():
     def __init__(self):
         self._pelit = {
-            "a": KPSPelaajaVsPelaaja(Tuomari()),
-            "b": KPSTekoaly(Tuomari(), Tekoaly()),
-            "c": KPSTekoaly(Tuomari(), TekoalyParannettu(10))
+            "a": lambda: KPSPelaajaVsPelaaja(Tuomari()),
+            "b": lambda: KPSTekoaly(Tuomari(), Tekoaly()),
+            "c": lambda: KPSTekoaly(Tuomari(), TekoalyParannettu(10))
             }
     def nayta(self):
         while True:
@@ -18,7 +18,7 @@ class UI():
             vastaus = input()
             print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
             if vastaus in self._pelit:
-                peli = self._pelit[vastaus]
+                peli = self._pelit[vastaus]()
                 peli.pelaa()
             else:
                 break
