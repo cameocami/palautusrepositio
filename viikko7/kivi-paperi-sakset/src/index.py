93f556e8863680ca836e5ce80fdf4d17abcee84e
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-12-21 18:19:36 +0200

    refactor ui into separate class

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -1,33 +1,9 @@
-from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
-from kps_tekoaly import KPSTekoaly
-from tekoaly import Tekoaly
-from tekoaly_parannettu import TekoalyParannettu
-from tuomari import Tuomari
+from ui import UI
 
 
-def main(tuomari=Tuomari(), tekoaly=Tekoaly(), tekoaly_parannettu=TekoalyParannettu(10)):
-    komennot = {
-    "a": KPSPelaajaVsPelaaja(tuomari),
-    "b": KPSTekoaly(tuomari, tekoaly),
-    "c": KPSTekoaly(tuomari, tekoaly_parannettu)
-    }
-    while True:
-
-        print("Valitse pelataanko"
-              "\n (a) Ihmistä vastaan"
-              "\n (b) Tekoälyä vastaan"
-              "\n (c) Parannettua tekoälyä vastaan"
-              "\nMuilla valinnoilla lopetetaan"
-              )
-
-        vastaus = input()
-
-        print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
-        if vastaus in komennot:
-            peli = komennot[vastaus]
-            peli.pelaa()
-        else:
-            break
+def main():
+    ui = UI()
+    ui.nayta()
 
 
 if __name__ == "__main__":
---
+++ viikko7/kivi-paperi-sakset/src/ui.py
@@ -0,0 +1,32 @@
+from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+from kps_tekoaly import KPSTekoaly
+from tekoaly import Tekoaly
+from tekoaly_parannettu import TekoalyParannettu
+from tuomari import Tuomari
+
+
+class UI():
+    def __init__(self):
+        self._pelit = {
+            "a": KPSPelaajaVsPelaaja(Tuomari()),
+            "b": KPSTekoaly(Tuomari(), Tekoaly()),
+            "c": KPSTekoaly(Tuomari(), TekoalyParannettu(10))
+            }
+    def nayta(self):
+        while True:
+            self._peli_valikko()
+            vastaus = input()
+            print("Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s")
+            if vastaus in self._pelit:
+                peli = self._pelit[vastaus]
+                peli.pelaa()
+            else:
+                break
+
+    def _peli_valikko(self):
+        print("Valitse pelataanko"
+                "\n (a) Ihmistä vastaan"
+                "\n (b) Tekoälyä vastaan"
+                "\n (c) Parannettua tekoälyä vastaan"
+                "\nMuilla valinnoilla lopetetaan"
+                )
