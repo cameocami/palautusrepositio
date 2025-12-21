360b181fe132500d86cfcf724ab56c851885eb95
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-12-21 18:56:03 +0200

    wrap score checking into separate method

--- viikko7/kivi-paperi-sakset/src/tuomari.py
+++ viikko7/kivi-paperi-sakset/src/tuomari.py
@@ -1,30 +1,35 @@
 
 # Luokka pitää kirjaa ensimmäisen ja toisen pelaajan pisteistä sekä tasapelien määrästä.
+EKA_VOITTAA = [("k", "s"), ("s", "p"), ("p", "k")]
+
+
 class Tuomari:
     def __init__(self):
-        self.ekan_pisteet = 0
-        self.tokan_pisteet = 0
-        self.tasapelit = 0
-        self._EKA_VOITTAA = [("k", "s"), ("s", "p"), ("p", "k")]
-
+        self._ekan_pisteet = 0
+        self._tokan_pisteet = 0
+        self._tasapelit = 0
+ 
     def kirjaa_siirto(self, ekan_siirto, tokan_siirto):
-        if self._tasapeli(ekan_siirto, tokan_siirto):
-            self.tasapelit = self.tasapelit + 1
-        elif self._eka_voittaa(ekan_siirto, tokan_siirto):
-            self.ekan_pisteet = self.ekan_pisteet + 1
+        tulos = self._tulos(ekan_siirto, tokan_siirto)
+
+        if tulos == "tasapeli":
+            self._tasapelit = self._tasapelit + 1
+        elif tulos == "eka_voittaa":
+            self._ekan_pisteet = self._ekan_pisteet + 1
         else:
-            self.tokan_pisteet = self.tokan_pisteet + 1
+            self._tokan_pisteet = self._tokan_pisteet + 1
 
     def __str__(self):
-        return f"Pelitilanne: {self.ekan_pisteet} - {self.tokan_pisteet}\nTasapelit: {self.tasapelit}"
+        return f"Pelitilanne: {self._ekan_pisteet} - {self._tokan_pisteet}\nTasapelit: {self._tasapelit}"
 
-    # sisäinen metodi joka tarkastaa voittaako eka pelaaja tokan
-    def _tasapeli(self, eka, toka):
-        return eka == toka
-    def _eka_voittaa(self, eka, toka):
-        return (eka, toka) in self._EKA_VOITTAA
+    def _tulos(self, eka, toka):
+        if eka == toka:
+            return "tasapeli"
+        if (eka, toka) in EKA_VOITTAA:
+            return "eka_voittaa"
+        return "toka_voittaa"
     
     def nollaa(self):
-        self.ekan_pisteet = 0
-        self.tokan_pisteet = 0
-        self.tasapelit = 0
+        self._ekan_pisteet = 0
+        self._tokan_pisteet = 0
+        self._tasapelit = 0
