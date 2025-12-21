9ed7bf3ac67f1a589ac5290920e3f17ef5583818
Camilla Campbell <camilla.campbell@helsinki.fi>
2025-12-21 17:21:29 +0200

    replace if-statement tree with winning moves dictionary

--- viikko7/kivi-paperi-sakset/src/tekoaly_parannettu.py
+++ viikko7/kivi-paperi-sakset/src/tekoaly_parannettu.py
@@ -3,6 +3,7 @@ class TekoalyParannettu:
     def __init__(self, muistin_koko):
         self._muisti = [None] * muistin_koko
         self._vapaa_muisti_indeksi = 0
+        self._voittotaulukko = { "k": "p", "p": "s", "s": "k" } # kivi -> paperi, paperi -> sakset, sakset -> kivi
 
     def aseta_siirto(self, siirto):
         # jos muisti täyttyy, unohdetaan viimeinen alkio
@@ -21,31 +22,20 @@ class TekoalyParannettu:
 
         viimeisin_siirto = self._muisti[self._vapaa_muisti_indeksi - 1]
 
-        k = 0
-        p = 0
-        s = 0
+        maarat = {"k": 0, "p": 0, "s": 0}
 
         for i in range(0, self._vapaa_muisti_indeksi - 1):
             if viimeisin_siirto == self._muisti[i]:
                 seuraava = self._muisti[i + 1]
-
-                if seuraava == "k":
-                    k = k + 1
-                elif seuraava == "p":
-                    p = p + 1
-                else:
-                    s = s + 1
+                maarat[seuraava] += 1
 
         # Tehdään siirron valinta esimerkiksi seuraavasti;
         # - jos kiviä eniten, annetaan aina paperi
         # - jos papereita eniten, annetaan aina sakset
         # muulloin annetaan aina kivi
-        if k > p or k > s:
-            return "p"
-        elif p > k or p > s:
-            return "s"
-        else:
-            return "k"
+        yleisin = max(maarat, key=maarat.get)
+
+        return self._voittotaulukko[yleisin]
 
         # Tehokkaampiakin tapoja löytyy, mutta niistä lisää
         # Johdatus Tekoälyyn kurssilla!
